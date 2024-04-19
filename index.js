6731a4ae16cfce4c6a6abe909f3df85a009d858c
PicToWeb <picronantoine34@gmail.com>
2024-04-19 12:50:25 +0200

    Test SSH

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.get("/", (req, res) => {
 app.listen(port, async () => {
   try {
     await sequelize.authenticate();
+    if (process.env.NODE_ENV !== "developpement") {
+      await sequelize.sync({ force: true });
+    }
+    console.log(process.env.NODE_ENV);
     console.log("Connection has been established successfully.");
   } catch (error) {
     throw error;
