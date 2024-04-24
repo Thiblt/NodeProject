c61c241422e597c64ef8399f33a9b576b4d19b18
Iam4ealDamba <prayforsouls720@gmail.com>
2024-04-24 14:21:06 +0200

    ajout client react

--- middlewares/beer_order.middleware.js
+++
@@ -1,9 +0,0 @@
-const { body, param } = require('express-validator');
-
-const validateId_biereParam = [
-    param('id_biere')
-      .notEmpty()
-      .isNumeric()
-  ];
-
-  module.exports = { validateId_biereParam };
--- models/beer_order.model.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require("sequelize");
-const db = require("../config/sequelize.config");
-
-const Beer_order = db.define("beer_order", {
-  id_beer: { type: sequelize.INTEGER, allowNull: false },
- id_order: { type: sequelize.INTEGER, allowNull: false },
-});
-
-module.exports = Beer_order;
